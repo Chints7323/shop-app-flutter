6a7bcc4d391252f8aac862f1c2823ad8ca03dfaf
Chints7323 <53396393+Chints7323@users.noreply.github.com>
2020-10-24 18:59:33 +0530

    Update product.dart

--- lib/provider/product.dart
+++ lib/provider/product.dart
@@ -29,7 +29,7 @@ class Product with ChangeNotifier {
     isFavorite = !isFavorite;
     notifyListeners();
     final url =
-        'https://light-router-280819.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
+        'https://light-router-280.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
     try {
       final response = await http.put(
         url,
